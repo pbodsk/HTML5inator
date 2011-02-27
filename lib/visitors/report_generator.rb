b983e20dd2f7680c48a78c55581f139197e7c794
pbodsk <me@peterbodskov.dk>
2011-02-27 21:38:11 +0100

    started building html report

--- lib/visitors/report_generator.rb
+++ lib/visitors/report_generator.rb
@@ -1,44 +1,79 @@
+require 'builder'
 module Visitors
   class ReportGenerator
+    attr_reader :report_elements
+    def initialize      
+      @report_elements = []   
+    end
+   
     def visitDoctype(doctype_command)
       if doctype_command.html5_doctype
-        "<p>You're already using the HTML5 doctype, good</p>"
+          @report_elements.push("You're already using the HTML5 doctype, good")
       else
-        "<p>Your doctype could be upgraded to <!DOCTYPE html></p>"
+          @report_elements.push("Your doctype could be upgraded to <!DOCTYPE html>")
       end
+      @report_elements.last
     end
     
     def visitTitle(title_command)
-      "<p>Title: #{title_command.title}</p>"
+        @report_elements.push("Title: #{title_command.title}")
+        @report_elements.last
     end
     
     def visitScriptTag(script_tag_command)
+      temp_builder = Builder::XmlMarkup.new(:indent => 2)
       if script_tag_command.scripts.length > 0
-        return_value = "<p>You no longer need to specify type=\"text/javascript\" on your &lt;script&gt; tags, so you can ditch that on these tags:</p>
-        <ul>"  
-        script_tag_command.scripts.each do |script|
-          return_value << "<li>" << script << "</li>"
-        end
-         return_value += "</ul>"
-        
+          temp_builder.p "You no longer need to specify type=\"text/javascript\" on your &lt;script&gt; tags, so you can ditch that on these tags:"
+          temp_builder.ul {
+            script_tag_command.scripts.each do |script|
+              temp_builder.li "#{script}"
+            end
+          }
+          
+          temp_elem = temp_builder.to_s
+          temp_elem = temp_elem[0,temp_elem.rindex("<to_s/>")]  
+          @report_elements.push(temp_elem)
+          temp_elem
       else
-        '<p>You have already omitted type="text/javascript" from your &lt;script&gt; tags, good</p>'
+        @report_elements.push("You have already omitted type=\"text/javascript\" from your &lt;script&gt; tags, good")
+        @report_elements.last
       end
     end
-    
+
     def visitCssTag(css_tag_command)
+      temp_builder = Builder::XmlMarkup.new(:indent => 2)
       if css_tag_command.styles.length > 0
-        return_value = "<p>You no longer need to specify type=\"text/css\" on your &lt;link&gt; tags, so you can ditch that on these tags:</p>
-        <ul>"  
-        css_tag_command.styles.each do |style|
-          return_value << "<li>" << style << "</li>"
-        end
-         return_value += "</ul>"
-        
+          temp_builder.p "You no longer need to specify type=\"text/css\" on your &lt;link&gt; tags, so you can ditch that on these tags:"
+          temp_builder.ul {
+            css_tag_command.styles.each do |style|
+              temp_builder.li "#{style}"
+            end            
+          }
+          temp_elem = temp_builder.to_s
+          temp_elem = temp_elem[0,temp_elem.rindex("<to_s/>")]
+          @report_elements.push(temp_elem)
+
+          temp_elem
       else
-        '<p>You have already omitted type="text/css" from your &lt;link&gt; tags, good</p>'
-      end
+         @report_elements.push("You have already omitted type=\"text/css\" from your &lt;link&gt; tags, good</p>")
+         @report_elements.last
+      end    
+    end    
+    
+    def generate_html
+
+      @builder = Builder::XmlMarkup.new(:indent => 2)
+      @builder.head {
+        @builder.title "Report"
+      }   
       
-    end
+      @builder.body {
+        @report_elements.each do |element|  
+          @builder.section element
+        end
+      }
+      @builder.html
+        
+    end    
   end
 end
--- test/test_report_generator.rb
+++ test/test_report_generator.rb
@@ -7,35 +7,38 @@ class TestDoctypeCommand < Test::Unit::TestCase
     @doctype_command = Commands::DoctypeCommand.new 
     @report_generator = Visitors::ReportGenerator.new
   end
-  
+
   def test_can_create
     refute_nil(@report_generator)
   end
   
   def test_can_accept_doctype_visitor_html5
     @page.doctype = "<!DOCTYPE html SYSTEM>"
-    @doctype_command.execute(@page)
-    assert_equal("<p>You're already using the HTML5 doctype, good</p>", @doctype_command.accept(@report_generator)) 
+    
+    refute_nil(/HTML5 doctype/ =~ @doctype_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
   
   def test_can_accept_doctype_visitor_not_html5
     @page.doctype = "<!DOCTYPE htmlfake SYSTEM>"
-    @doctype_command.execute(@page)
-    assert_equal("<p>Your doctype could be upgraded to <!DOCTYPE html></p>", @doctype_command.accept(@report_generator)) 
+    
+    refute_nil(/upgraded/ =~ @doctype_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
   
   def test_can_accept_title_visitor
     @title_command = Commands::TitleCommand.new
     refute_nil(@title_command)
-    @title_command.execute(@page)
-    assert_equal("<p>Title: Mocked title</p>", @title_command.accept(@report_generator))
+
+    refute_nil(/Mocked title/ =~ @title_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
   
   def test_can_accept_script_tag_visitor_on_scripts_with_type_set
     @script_tag_command = Commands::ScriptTagCommand.new
     refute_nil(@script_tag_command)
-    @script_tag_command.execute(@page)
-    refute_nil(/<li>/ =~ @script_tag_command.accept(@report_generator))
+    refute_nil(/<li>/ =~ @script_tag_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
   
   def test_can_accept_script_tag_visitor_on_scripts_without_type_set
@@ -43,16 +46,16 @@ class TestDoctypeCommand < Test::Unit::TestCase
     refute_nil(@script_tag_command)
     @page.mock_script = ["&lt;script src='blah.js'&gt; &lt;script&gt;"]
     
-    @script_tag_command.execute(@page)
-    
-    assert_equal('<p>You have already omitted type="text/javascript" from your &lt;script&gt; tags, good</p>', @script_tag_command.accept(@report_generator))
+    refute_nil(/You have already omitted/ =~ @script_tag_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
 
   def test_can_accept_css_tag_visitor_on_css_with_type_set
     @css_tag_command = Commands::CssTagCommand.new
     refute_nil(@css_tag_command)
-    @css_tag_command.execute(@page)
-    refute_nil(/<li>/ =~ @css_tag_command.accept(@report_generator))
+
+    refute_nil(/<li>/ =~ @css_tag_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
 
   def test_can_accept_css_tag_visitor_on_css_without_type_set
@@ -60,9 +63,24 @@ class TestDoctypeCommand < Test::Unit::TestCase
     refute_nil(@css_tag_command)
     @page.mock_style = ["&lt;link href='blah.css'&gt; /&gt;"]
     
-    @css_tag_command.execute(@page)
-    
-    assert_equal('<p>You have already omitted type="text/css" from your &lt;link&gt; tags, good</p>', @css_tag_command.accept(@report_generator))
+    refute_nil(/You have already omitted/ =~ @css_tag_command.execute_and_accept(@page, @report_generator))
+    assert_equal(@report_generator.report_elements.size, 1)
   end
-  
+
+  def test_can_build_html
+    @css_tag_command = Commands::CssTagCommand.new
+    @script_tag_command = Commands::ScriptTagCommand.new
+    @title_command = Commands::TitleCommand.new
+    
+    @doctype_command.execute_and_accept(@page, @report_generator)
+    @title_command.execute_and_accept(@page, @report_generator)
+    @css_tag_command.execute_and_accept(@page, @report_generator)
+    @script_tag_command.execute_and_accept(@page, @report_generator)
+    
+    output = @report_generator.generate_html
+    puts output
+    refute_nil(/text\/javascript/ =~ output)
+    refute_nil(/text\/css/ =~ output)
+ 
+  end
 end
